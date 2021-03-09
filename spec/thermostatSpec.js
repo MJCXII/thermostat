e5942648505b2ad183078a63b1612e607577b897
Maximilian Clarke <maximilian-jc@live.com>
2021-03-09 15:11:35 +0000

    temp can be adjusted up, test for down function implemented

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,12 +1,19 @@
 describe('Thermostat', function() {
-  let thermostat = new Thermostat();
 
   it('temperature starts at 20', function() {
+      let thermostat = new Thermostat();
       expect(thermostat.temperature).toEqual(20);
   })
 
   it('can increase the temperature with an up function', function() {
+      let thermostat = new Thermostat();
       thermostat.up(10)
       expect(thermostat.temperature).toEqual(30);
+  })
+
+  it('can decrease the temperature with a down function', function() {
+      let thermostat = new Thermostat();
+      thermostat.down(10)
+      expect(thermostat.temperature).toEqual(10);
   })
 })
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,4 +2,9 @@ class Thermostat {
   constructor(temperature = 20) {
     this.temperature = temperature
   }
+
+  up(temp) {
+    this.temperature += temp;
+  }
+
 }
