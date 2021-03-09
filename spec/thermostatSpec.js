9f81e9ec695108c44e6b752b3e54964170352d2c
ianmcnicholas <ianjmcnicholas@.com>
2021-03-09 15:06:32 +0000

    Thermostat initiates with temperature of 20.  New test - there should be an up function

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,8 +1,12 @@
 describe('Thermostat', function() {
+  let thermostat = new Thermostat();
+
   it('temperature starts at 20', function() {
-
-      let thermostat = new Thermostat();
       expect(thermostat.temperature).toEqual(20);
+  })
 
+  it('can increase the temperature with an up function', function() {
+      thermostat.up(10)
+      expect(thermostat.temperature).toEqual(30);
   })
 })
